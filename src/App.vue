c21617b62c5e959743f7a940fbaf45210a034714
DionisiePulbere <dionisiepulbere@gmail.com>
2024-04-17 01:49:13 +0200

    feat: add option per filtrare

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,60 @@ import axios from 'axios';
 import { store } from './store.js';
 import AppHeader from './components/AppHeader.vue';
 import AppCardsList from './components/AppCardsList.vue';
+import AppFilter from './components/AppFilter.vue';
 
 export default{
   components: {
     AppHeader,
     AppCardsList,
+    AppFilter,
   },
   data () {
     return{
-      store
+      queryParams: {
+        num: 20,
+        offset: 0
+      }
     }
   },
   methods: {
     getCardsFromApi() {
-      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php", { params: this.queryParams })
       .then((response) => {
-        console.log(response.data.data);
         store.cards = response.data.data;
       });
+    },
+
+    getTypeFromApi(){
+      axios.get("https://db.ygoprodeck.com/api/v7/archetypes.php")
+      .then((response) => {
+        store.typeList = response.data;
+      })
+    },
+    filterTypeApi(){
+      if (store.filterType !== ""){
+        this.queryParams.archetype = store.filterType;
+      } else {
+        this.queryParams.archetype = "";
+      };
+      axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php", { params: this.queryParams })
+				.then((response) => {
+				store.cards = response.data.data;
+				})
     }
   },
+
   mounted() {
-    this.getCardsFromApi()
-  },
+    this.getCardsFromApi(),
+    this.getTypeFromApi()
+  }
 }
 </script>
 
 <template>
   <AppHeader></AppHeader>
   <main class="p-5">
+    <AppFilter @filterType="filterTypeApi"></AppFilter>
     <AppCardsList></AppCardsList>
   </main>
 </template>
---
+++ src/components/AppFilter.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    name: 'AppFilter',
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container mb-5">
+        <select v-model="store.filterType" @change="$emit('filterType')">
+            <otpion value="" selected>Seleziona un filtro</otpion>
+            <option v-for="archetype in store.typeList" :value="archetype.archetype_name"> {{ archetype.archetype_name }} </option>
+        </select>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
